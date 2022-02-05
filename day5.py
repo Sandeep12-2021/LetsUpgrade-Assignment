6436217480887a4dc0ae949e1b17e0f8f5cba28d
Sandeep Murthy <96412573+Sandeep12-2021@users.noreply.github.com>
2022-02-05 08:37:27 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -4,4 +4,4 @@ ls = list(range(1,101))
 ls3 = list(filter(lambda x:(x%3==0),ls))
 ls5 = list(filter(lambda x:(x%5==0),ls))
 
-print("List of elements divisible by 3:",ls3,"\n""List of elements divisible by 5:",ls5)
+print("List of elements divisible by 3:",ls3,"\n List of elements divisible by 5:",ls5)
