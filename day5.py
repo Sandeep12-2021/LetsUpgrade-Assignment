367c69d3bc5131a3f507c4f4c2c473ee1b21cff5
Sandeep Murthy <96412573+Sandeep12-2021@users.noreply.github.com>
2022-02-05 08:34:51 +0530

    PYTHON DAY5 ASSIGMENT DONE

---
+++ day5.py
@@ -0,0 +1,7 @@
+ls = list(range(1,101))
+
+# generating lists that are having elements divisible by 3 and 5 using filter func
+ls3 = list(filter(lambda x:(x%3==0),ls))
+ls5 = list(filter(lambda x:(x%5==0),ls))
+
+print("List of elements divisible by 3:",ls3,"\n""List of elements divisible by 5:",ls5)
